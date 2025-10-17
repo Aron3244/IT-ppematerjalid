ac0496b2f40e08949d6fac92ae2f5ef8c30f98df
Opilane TTHK <artjom.spesivcev@gmail.com>
2025-10-17 09:19:14 +0300

    Update Program.cs

--- 05. 17.10.25/Swicth/Swicth/Program.cs
+++ 05. 17.10.25/Swicth/Swicth/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sisesta vokaal ja vajuta enter");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "a":
+                     Console.WriteLine("Vokaal a");
+                    break;
+
+                case "e":
+                     Console.WriteLine("Vokaal a");
+                    break;
+
+                case "i":
+                     Console.WriteLine("Vokaal i");
+                    break;
+
+                default:
+                     Console.WriteLine("Muu täht");
+                    break;
+                
+            }
         }
     }
 }
+
+
