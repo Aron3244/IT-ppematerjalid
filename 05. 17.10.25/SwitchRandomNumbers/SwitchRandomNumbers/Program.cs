30e5e717a2fa5e6df93aa7afaa9bf9fd7da3b8b3
Opilane TTHK <artjom.spesivcev@gmail.com>
2025-10-17 10:30:39 +0300

    SwitchRandomNumber

---
+++ 05. 17.10.25/SwitchRandomNumbers/SwitchRandomNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchRandomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("genereerib numbreid ühest kuni kuueni");
+
+            //Kõik kuus numbrit ära tuleb käsitleda swichiga
+
+            int cube = new Random().Next(1, 7);
+
+            switch (cube)
+            {
+                case 1:
+                    Console.WriteLine("Tuli 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuli 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuli 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Tuli 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Tuli 5");
+                    break;
+                case 6:
+                    Console.WriteLine("Tuli 6");
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
