11d15481c108cc48a22767609223533a4dc69d7e
Opilane TTHK <opilane@tthk.ee>
2025-10-31 11:22:22 +0200

    SwitchMetohodCall

---
+++ 06. 31.10.25/SwitchMetohodCall/SwitchMetohodCall/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SwitchMetohodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta CarCoupe või truck");
+            //teha switch rakendus
+            //kus kasutaja sisestab sõiduki tüübi (coupe või truck)
+            //ja programm kutsub vastava meetodi (CarCoupe või Truck)
+
+            string vehicleType = Console.ReadLine().ToLower();
+
+            switch (vehicleType)
+            {
+                case "CarCoupe":
+                    CarCoupe();
+                    break;
+
+                case "truck":
+                    Truck();
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR!");
+                    break;
+            }
+        }
+
+        static void CarCoupe()
+        {
+            Console.WriteLine("This is a coupe car.");
+        }
+
+        static void Truck()
+        {
+            Console.WriteLine("This is a truck.");
+        }
+    }
+}
+
