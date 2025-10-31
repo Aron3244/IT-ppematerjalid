83a6e86dae919f169a24a760674c84f37c025b81
Opilane TTHK <opilane@tthk.ee>
2025-10-31 09:55:17 +0200

    SwitchMultipleCases

---
+++ 06. 31.10.25/SwitchMultipleCases/SwitchMultipleCases/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SwitchMultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //sisestad konsooli väärtuse
+            //loeb sisestatud väärtuse stringina
+            //esimese cases on väärtus "Intro to C#", "Variables",
+            //"Data Types" ja vastuseks Basic
+            //teises cases on väärtus "OOP", "Classes"
+            //"Objects ja vastuseks Intermediate
+            //kolmandas cases on väärtus "Asynchronous Programming", "LINQ",
+            //"Delegates" ja vastuseks Advanced
+           
+                Console.WriteLine("Sisesta teema nimi:");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "Intro to C#" or "Variables" or "Variables":
+                        Console.WriteLine("Basic");
+                        break;
+                    
+                    case "OOP" or "Classes" or "Objects":
+                    Console.WriteLine("Intermediate");
+                        break;
+
+                    case "Asynchronous Programming" or "LINQ" or "Delegates":
+                        Console.WriteLine("Advanced");
+                        break;
+
+                    default:
+                        Console.WriteLine("Tundmatu tase");
+                        break;
+                }
+            }
+        }
+    }
+
+
