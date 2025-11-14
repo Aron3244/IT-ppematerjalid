0a45b725db20c9234b658fa58488ad14c87f4011
Opilane TTHK <opilane@tthk.ee>
2025-11-14 09:42:32 +0200

    SwitchCalculator2

--- 07. 14.11.25/SwitchCalculator/SwitchCalculator/Program.cs
+++ 07. 14.11.25/SwitchCalculator/SwitchCalculator/Program.cs
@@ -5,45 +5,49 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Sisesta esimene number");
-            //loeme kasutaja sisendit ja teisendameselle float tüübiks
-            float num1 = float.Parse(Console.ReadLine());
+            //loeme kasutaja sisendit ja teisendame selle float tüübiks
+            float firstNumber = float.Parse(Console.ReadLine());
 
             //konsool kirjutab kasutajale, et vali tehe: +, -, *, /
-            Console.WriteLine("vali tehe: +, -, *, /");
-            string op = Console.ReadLine();
+            Console.WriteLine("Vali tehe: +, -, *, /");
+            string operation = Console.ReadLine();
 
-            //konsool kirjutab, et seisesta teine number ja teisendame selle float tüübiks
+            //konsool kirjutab, et sisesta teine number ja teisendame selle float tüübiks
             Console.WriteLine("Sisesta teine number");
-            float num2 = float.Parse(Console.ReadLine());
+            float secondNumber = float.Parse(Console.ReadLine());
 
             //teeme switch lausega tehte valiku
-            float result = 0;
 
-           
+            switch (operation)
             {
-                switch (op)
-                {
-                    case "+":
-                    result = num1 + num2;
+                case "+":
+                    Console.WriteLine($"Tulemus: {firstNumber + secondNumber}");
                     break;
-                case "-":
-                    result = num1 - num2;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    break;
-                case "/":
-                        if (num2 == 0)
-                            Console.WriteLine("Error");
-                        else
-                        {
-                            result = num1 / num2;
-                        }
-                        break;
-                }
-                Console.WriteLine("Vastus on " + result);
 
+                case "-":
+                    Console.WriteLine($"Tulemus: {firstNumber - secondNumber}");
+                    break;
+
+                case "*":
+                    Console.WriteLine($"Tulemus: {firstNumber * secondNumber}");
+                    break;
+
+                case "/":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("Nulliga jagamine ei ole lubatud!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Tulemus: {firstNumber / secondNumber}");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Tundmatu tehe!");
+                    break;
             }
+
         }
     }
 }
