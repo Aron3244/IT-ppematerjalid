a1313208dc4abeef03623d1939d3a2ea959a1d37
Aron3244 <Aadupill05@gmail.com>
2025-11-21 09:46:19 +0200

    TryCatchSwitch

---
+++ 08. 21.11.25/TryCatchSwitch/TryCatchSwitch/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TryCatchSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try catch Switch");
+
+            //tee switch, kus on ainult kaks casei 
+            //ja valikud on 1 ja 2
+            //defualt haru ei ole vajalik
+            try
+            {
+
+            
+
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Sisestasid ühe");
+                    break;
+                    
+                case 2:
+                    Console.WriteLine("Sisestasid kahe");
+                    break;
+            }
+        }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta number!");
+
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või liiga väike!");
+            }
+
+        }
+    }
+}
