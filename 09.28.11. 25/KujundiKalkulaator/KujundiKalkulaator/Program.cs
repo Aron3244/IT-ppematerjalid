ac2377f9045bd3e9dd69e35c8d904b863957c995
Aron3244 <Aadupill05@gmail.com>
2025-11-28 11:02:01 +0200

    KujundiKalkulaator

---
+++ 09.28.11. 25/KujundiKalkulaator/KujundiKalkulaator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            //Ruudu sees asub ring. Ringi raadius on 3 ühikut.
+            //Leia ja väljasta ekraanile ruudi pindala ja ümbermõõt
+           
+
+            int roundRadius = 3;
+            int diameter = roundRadius * 2;
+            double squareArea = diameter * diameter;
+            double squarePerimeter = diameter * 4;
+
+            Console.WriteLine("Ruudu pindala: " + squareArea);
+            Console.WriteLine("Ruudu ümbermõõt: " + squarePerimeter);
+
+            //arvutada ringi pindala ja ümbermõõt
+
+            double cercleArea = Math.PI * (roundRadius * roundRadius);
+            double cerclePerimeter = 2 * Math.PI * roundRadius;
+
+            Console.WriteLine("Ringi pindala: " + cercleArea);
+            Console.WriteLine("Ringi ümbermõõt: " + cerclePerimeter);
+        }
+    }
+}
