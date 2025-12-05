d6022923a3b51d6258656ff06e8a699d47c7a08d
Opilane TTHK <opilane@tthk.ee>
2025-12-05 10:35:32 +0200

    ForAsterisk

---
+++ 10.05.12. 25/ForAsterisk/ForAsterisk/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ForAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Asterisk");
+
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            //tuleb kasutada for loopi
+            //sisestan ridade arvuja tuleb vastav ridade arn konsooli
+            //pean näirama tärne
+
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine("*");
+            }
+            
+        }
+    }
+}
