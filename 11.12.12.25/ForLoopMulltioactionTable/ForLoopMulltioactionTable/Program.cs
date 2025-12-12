a920826a7e4b282bd2b06945dc8245bd9f5c54b1
Opilane TTHK <opilane@tthk.ee>
2025-12-12 10:52:12 +0200

    ForLoopMultipcationTable

---
+++ 11.12.12.25/ForLoopMulltioactionTable/ForLoopMulltioactionTable/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ForLoopMultipcationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Korrutustabel");
+
+            int j, i, n;
+
+                Console.WriteLine("Sisesa tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Korrutustable ühest kuni {0}", n);
+            Console.WriteLine("\n");
+
+            for (i = 1; i <= 10; i++)
+            {
+               for (j = 1; j <= n; j++)
+                {
+                    if (j<=  n- 1) 
+                    {
+                        Console.Write("{0}x{1} = {2}, ",j, i, i * j);
+                    }
+                    else
+                    {
+                        Console.Write("{0}x{1} = {2}, ", j, i, i * j);
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
