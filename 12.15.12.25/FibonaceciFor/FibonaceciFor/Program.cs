1612718e12facdf5433c413c9a606d85c8a15821
Opilane TTHK <opilane@tthk.ee>
2025-12-15 09:58:07 +0200

    FibonaceciFor

---
+++ 12.15.12.25/FibonaceciFor/FibonaceciFor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace FibonaceciFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("N`ita esimest Fibonacci seeriat");
+            Console.WriteLine("-------------------------------\n\n");
+            Console.WriteLine("Sisesta number");
+            int prv = 0, pre = 1, trm, i, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Siin on Fibonacci seeria {0} kuni \n", n);
+            Console.Write($"{0} {1}", prv, pre);
+
+            for (i  = 3; i <= n; i++)
+            {
+                trm = prv + pre;
+                Console.Write("{0} ", trm);
+                prv = pre;
+                pre = trm;
+            }
+            Console.Write("\n");
+        }
+    }
+}
