8d5c265d599f61a1d346f1a880c3a3badb110d69
Opilane TTHK <opilane@tthk.ee>
2026-01-09 10:32:21 +0200

    Ruutvõrrand

---
+++ 14.09.01.26/Ruutvõrrand/Ruutvõrrand/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ruutvõrrand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // ax2 + bx + c
+
+            // sisesta a, b ka c väärtus
+            // kasutaja peab saama sisestada neid väärtuseid
+
+            Console.WriteLine("Ruutvõrrand ax² + bx + c = 0");
+
+            Console.Write("Sisesta a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Sisesta b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Sisesta c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double vastus = b * b - 4 * a * c;
+
+            Console.WriteLine(
+                $"x1 = {(-b + Math.Sqrt(vastus)) / (2 * a)}, " +
+                $"x2 = {(-b - Math.Sqrt(vastus)) / (2 * a)}"
+            );
+        }
+    }
+}
