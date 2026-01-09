9caf6d5c566649c76a4c696fbed4d95ae95b93fd
Opilane TTHK <opilane@tthk.ee>
2026-01-09 11:06:21 +0200

    square

---
+++ 14.09.01.26/Ruut/Ruut/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ruut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //teha forl oopiga ruut
+            Console.WriteLine("Sisest ruudu suurus");
+            int size = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            //kui suurus on sisestatud, siis kuvab meile ka ümbermõõdu ja pindala
+
+            Console.WriteLine("\n");
+            int squarearea = size * size;
+            int squareAround = size * 4;
+
+
+            Console.WriteLine("Ruudu pindala: " + squarearea);
+            Console.WriteLine("Ruudu ümbermõõt: " + squareAround);
+        }
+    }
+}
+        
+    
