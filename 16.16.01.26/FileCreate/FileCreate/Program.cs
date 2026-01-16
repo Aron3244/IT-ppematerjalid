a2833bea07822a65762af4e7dc0b0da9028d22f7
Opilane TTHK <opilane@tthk.ee>
2026-01-16 10:43:30 +0200

    FileCreate

---
+++ 16.16.01.26/FileCreate/FileCreate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FileCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjutab faili läbi konsooli");
+
+            string filePath = "C:/Users/opilane/Desktop/WriteFile.txt";
+            string input = Console.ReadLine();
+
+            File.WriteAllText(filePath, input);
+        }
+    }
+}
